263e91f7efb42f34563d7196a508a5d323a667d6
akito23 <67802974+akito23@users.noreply.github.com>
2020-08-13 16:16:20 +0300

    Create Solution.cpp

---
+++ Solution.cpp
@@ -0,0 +1,52 @@
+class CombinationIterator 
+{
+    private:
+        std::string C;
+        std::string Comb;
+        int Position;
+        int L;
+        std::vector< std::string > PossibleComb;
+        
+        inline void FindPossibleCobinations(int Start, int End, int Index) noexcept
+        {
+            if( Index == L )
+            {
+                PossibleComb.push_back( std::move( std::string(Comb) ) );
+                return;  
+            }   // if
+            
+            for ( int i = Start; i <= End && End - i + 1 >= L - Index; i++ )  
+            {  
+                Comb[Index] = C[i];  
+                FindPossibleCobinations( i + 1, End, Index + 1 );  
+            }  // for
+        }   // FindPossibleCobinations
+    
+    public:
+        inline CombinationIterator(string c, int l) noexcept
+            : C(c), L(l), Position(0)
+        {
+            for( int i = 0; i < l; ++i )
+                Comb += '\0';
+                
+            FindPossibleCobinations( 0, C.length() - 1, 0 );
+        }  // Constructor
+        
+        inline ~CombinationIterator() noexcept
+        {
+            Comb = "";
+            C = "";
+            Position = L = 0;
+            PossibleComb.clear();
+        }   // Destructor
+    
+        std::string next() { return PossibleComb[Position++]; }   // next
+        bool hasNext() { return PossibleComb.size() != Position; }  // hasNext
+};
+
+/**
+ * Your CombinationIterator object will be instantiated and called as such:
+ * CombinationIterator* obj = new CombinationIterator(characters, combinationLength);
+ * string param_1 = obj->next();
+ * bool param_2 = obj->hasNext();
+ */
